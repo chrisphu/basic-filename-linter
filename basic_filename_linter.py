dadbe1a551b3ce38f95151bb16d254d5cae05e38
Chris Phu <67293666+chrisphu@users.noreply.github.com>
2023-11-14 22:22:55 -0600

    Switched underscore and hyphen

--- basic_filename_linter.py
+++ basic_filename_linter.py
@@ -23,7 +23,7 @@ def rename_files_in_directory(directory_path):
             continue
         current_file_name = os.path.basename(path)
         current_file_path = path.absolute()
-        new_file_name = current_file_name.lower().replace(' ', '_').replace('-', '_')
+        new_file_name = current_file_name.lower().replace(' ', '-').replace('_', '-')
         new_file_path = os.path.join(directory_path.absolute(), new_file_name)
         os.rename(current_file_path, new_file_path)
 
